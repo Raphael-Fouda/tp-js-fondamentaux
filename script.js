4940ce4a533d9d821e1b6b522c842f2c14a84d7a
Raphael-Fouda <fouda0582@gmail.com>
2025-11-04 15:05:24 +0000

    feat: Maîtrise des variables et conditions

--- script.js
+++ script.js
@@ -1,2 +1,15 @@
 console.log("Laboratoire prêt !");
-
+const nom = "Fouda";
+let age = 17;
+//nom = "Durand";
+age = 18;
+console.log(age);
+const motDePasseAttendu = "secret123";
+let motDePasseUtilisateur = "trop"; // Changez cette valeur pour tester
+if (motDePasseAttendu === motDePasseUtilisateur) {
+    console.log("Accès autorisé. Bienvenue !");
+    } else if (motDePasseUtilisateur.length<8) {
+         console.log("Accès refusé. Votre mot de passe est trop court (8 caractères min).");
+    } else {
+         console.log("Accès refusé. Mot de passe incorrect.");
+    }   
